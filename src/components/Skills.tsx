b75ac86acd853401d8e92a4969b62585c34af03c
Machel G.O.O <machelgabriel@gmail.com>
2025-06-03 01:58:43 +0300

    Chore: Update Skills component with improved styling and heading

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,8 +1,10 @@
 const Skills = () => {
   return (
     <>
-      <div>
-        <h2>This is Skills section.</h2>
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-4xl font-bold text-center text-indigo-400 mb-12">
+          My Skills
+        </h2>
       </div>
     </>
   );
